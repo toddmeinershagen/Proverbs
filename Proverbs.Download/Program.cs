49a96990174a7439627f17eb1f21c5e7acc8f423
Todd Meinershagen <todd@meinershagen.net>
2013-09-03 05:44:21 -0500

    initial commit

---
+++ Proverbs.Download/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using Dapper;
+
+namespace Proverbs.Download
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var parser = new VerseParser();
+            var gateway = new ScriptureGateway("8eeffb3fb3632fbe", parser);
+            var repository = new ScriptureRepository();
+            var service = new Service(gateway, repository);
+            service.Execute();
+        }
+    }
+
+    public class Service
+    {
+        private readonly IScriptureGateway _scriptureGateway;
+        private readonly IScriptureRepository _scriptureRepository;
+
+        public Service(IScriptureGateway scriptureGateway, IScriptureRepository scriptureRepository)
+        {
+            _scriptureGateway = scriptureGateway;
+            _scriptureRepository = scriptureRepository;
+        }
+
+        public void Execute()
+        {
+            var book = _scriptureGateway.GetBook("Proverbs");
+            _scriptureRepository.Save(book);
+        }
+    }
+
+    public interface IScriptureGateway
+    {
+        Book GetBook(string bookName);
+    }
+
+    public class ScriptureGateway : IScriptureGateway
+    {
+        private readonly string _key;
+        private readonly VerseParser _parser;
+
+        public ScriptureGateway(string key, VerseParser parser)
+        {
+            _key = key;
+            _parser = parser;
+        }
+
+        public Book GetBook(string bookName)
+        {
+            var text = "";
+            var verses = _parser.Parse(text);
+            return null;
+        }
+    }
+
+    public interface IScriptureRepository
+    {
+        void Save(Book book);
+    }
+
+    public class ScriptureRepository : IScriptureRepository
+    {
+        public void Save(Book book)
+        {
+            const string connectionString = "";
+            using (IDbConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                var rowCount = connection.Execute(@"insert MyTable(colA, colB) values (@a, @b)", book.Verses);
+                Trace.Assert(rowCount == book.Verses.Count);
+            }
+        }
+    }
+
+    public class Book
+    {
+        public List<Verse> Verses { get; set; }
+    }
+
+    public class Verse
+    {
+        public Int16 ChapterNumber { get; set; }
+        public Int16 VerseNumber { get; set; }
+        public string Text { get; set; }
+    }
+}
---
+++ Proverbs.Download/VerseParser.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Proverbs.Download
+{
+    public class VerseParser
+    {
+        public List<Verse> Parse(string text)
+        {
+            const string pattern = @"\[.*\]";
+            var substrings = Regex.Split(text, pattern);
+            var openingBracketIndex = text.IndexOf('[');
+            var colonIndex = text.IndexOf(':');
+            var closingBracketIndex = text.IndexOf(']');
+            var chapter = text.Substring(openingBracketIndex + 1, colonIndex - openingBracketIndex - 1);
+            var startingChapter = Convert.ToInt16(chapter);
+            var verse = text.Substring(colonIndex + 1, closingBracketIndex - colonIndex - 1);
+            var startingVerse = Convert.ToInt16(verse);
+
+            var verses = (from match in substrings
+                          where !string.IsNullOrEmpty(match)
+                          select new Verse { ChapterNumber = startingChapter, Text = match.Trim(), VerseNumber = startingVerse++ });
+
+            return verses.ToList();
+        }
+    }
+}
